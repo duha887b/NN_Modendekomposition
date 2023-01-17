8587577c20ca00f969512b8d46d8e03ea8d5e554
Dustin <dustin.hanusch@mailbox.tu-dresden.de>
2023-01-17 12:33:37 +0100

    update

--- Exercise_3_MATLAB/dataset_generation.m
+++ Exercise_3_MATLAB/dataset_generation.m
@@ -8,16 +8,36 @@ image_size = 32;    % resolution 32x32
  
 %% generation of complex mode weights and label vector - step 1
 
-% 1. create random amplitude weights. The weights of amplitude should be normalized.
 
-% 2. create random phase amplitude. (Using realtive phase difference)
 
-% 3. complex mode weights vector
 
-% 4. normalize cos(phase) to (0,1)
 
-% 5. combine amplitude and phase into a label vector (1,2N-1)
 
+
+
+for n=1 : number_of_data
+
+    rho = rand(1,number_of_modes);                              % 1. create random amplitude weights. The weights of amplitude should be normalized.
+    rho_n = rho/norm(rho);
+
+    phase = rand(1,number_of_modes)*2*pi;                       % 2. create random phase amplitude. (Using realtive phase difference)
+    phaes_rel = phase - phase(1);
+    phase_norm = cos(phaes_rel);    
+    phase_scale = rescale(phase_norm,InputMin=-1,InputMax=1);   % 4. normalize cos(phase) to (0,1)
+    k_i = rho_n.*exp(1i*phase_scale);                           % 3. complex mode weights vector
+                                                            
+                                                            
+
+    label = [rho_n phase_scale(:,2:size(phase_scale,2))];
+    
+    lebel_vec(:,n) = label; 
+    complexMode_vec(:,n) = k_i; 
+end
+
+
+
+
+%dividerand
 % 6. split complex mode weights vector and label vector into Training,
 % validation and test set. 
 
